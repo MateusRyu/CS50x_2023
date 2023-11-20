2c226129b27f13a101c5f2f76c2368fdfd2b35c2
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2023-11-20 16:45:43 -0300

    feat: Add solution to challenge 11 of the Problem Set 7: Movies

---
+++ problem_set/problem_set_7/Movies/11.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT movies.title FROM movies
+    JOIN ratings
+        ON ratings.movie_id = movies.id
+    JOIN stars
+        ON movies.id = stars.movie_id
+    JOIN people
+        ON stars.person_id = people.id
+    WHERE people.name = 'Chadwick Boseman'
+    ORDER by ratings.rating DESC
+    LIMIT 5;
